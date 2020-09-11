9904454ce2c1f1163cc0f1efa716b72cc0d87713
simpletextbr <simpletextbr@gmail.com>
2020-09-11 20:42:17 -0300

    First Commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+
+const routes = require("./routes");
+
+const app = express();
+
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(process.env.PORT || 3333);
